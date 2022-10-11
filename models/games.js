6688eaf45adc3911cd9f02ce455da24f4c1460f5
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-10-11 14:21:50 -0400

    added schema for games

--- models/games.js
+++ models/games.js
@@ -4,5 +4,11 @@ const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI)
 
 const gamesSchema = new mongoose.Schema({
-
+    location: {type: String, required: true},
+    time: {type: String, required: true},
+    playersNeeded: {type: Number, required: true}
 })
+
+const Game = mongoose.model('Game', gamesSchema)
+
+module.exports= Game
