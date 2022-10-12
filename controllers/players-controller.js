542178242a27ada5beabaeb4cb71b899f6249cf2
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-10-12 10:05:23 -0400

    routes for players done

--- controllers/players-controller.js
+++ controllers/players-controller.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.get('/:gameId', (req, res) => {
+router.get('/:gameId', async (req, res) => {
     try {
         const players = await allPlayers.find({game: req.params.gameId})
         res.json(players)
@@ -21,3 +21,35 @@ router.get('/:gameId', (req, res) => {
     }
 })
 
+router.get('/player/:playerId', async (req, res) => {
+    try {
+        const player = await allPlayers.findById(req.params.playerId)
+        res.json(player)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        res.json(await allPlayers.create(req.body))
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.put('/player/:playerId', async (req, res) => {
+    try {
+        res.json(await allPlayers.findByIdAndUpdate(req.params.playerId, req.body))
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.delete('/:playerId', async (req, res) => {
+    try {
+        res.json(await allPlayers.findByIdAndDelete(req.params.playerId))
+    } catch (error) {
+        console.log(error)
+    }
+})
