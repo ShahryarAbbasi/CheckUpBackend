7766df241e839a095fc11f5679fbddcf43794af6
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-10-15 14:35:25 -0400

    update to game model

--- models/games.js
+++ models/games.js
@@ -6,15 +6,10 @@ mongoose.connect(mongoURI)
 const gamesSchema = new mongoose.Schema({
     location: {type: String, required: true},
     time: {type: String, required: true},
-    playersNeeded: {type: Number, required: true}
+    players: {type: Number, required: true}
 })
 
 const Game = mongoose.model('Game', gamesSchema)
 
 module.exports = Game
 
-Game.create({
-    location: 'MAC',
-    time: 'today',
-    playersNeeded: 9
-})
